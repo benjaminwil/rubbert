c0ce55d184888d63253a965f06737c5dd4bcd2cf
benjamin wil <hey@benjaminwil.info>
2023-04-01 15:48:46 -0700

    resolve player-over-edge bug
    this commit refactors the controller class and resolves an issue where
    the first time an arrow key was pressed, the player would move forever
    and fall off the edge of the `world.tiles` area.

--- app/models/player_controller.rb
+++ app/models/player_controller.rb
@@ -12,46 +12,41 @@ class PlayerController
 
   def control! args
     case
-    when key_down(args).up then    player.state.nav.up = args.tick_count
-    when key_down(args).right then player.state.nav.right = args.tick_count
-    when key_down(args).down then  player.state.nav.down = args.tick_count
-    when key_down(args).left then  player.state.nav.left = args.tick_count
+    when key_down(args).up then go_north args, nav: player.state.nav
+    when key_down(args).right then go_east args, nav: player.state.nav
+    when key_down(args).down then go_south args, nav: player.state.nav
+    when key_down(args).left then go_west args,  nav: player.state.nav
     end
-
-    go_north tick_count: args.tick_count, nav: player.state.nav
-    go_east  tick_count: args.tick_count, nav: player.state.nav
-    go_south tick_count: args.tick_count, nav: player.state.nav
-    go_west  tick_count: args.tick_count, nav: player.state.nav
   end
 
-  def go_north(nav:, tick_count:)
+  def go_north(args, nav:)
+    player.state.nav.up = args.tick_count
     go nav: nav,
        nav_direction: nav&.up,
-       tick_count: tick_count,
        x_range: (player.x + 1..player.x + tile_width),
        y_range: (player.y + 1..player.y + tile_height)
   end
 
-  def go_east(nav:, tick_count:)
+  def go_east(args, nav:)
+    player.state.nav.right = args.tick_count
     go nav: nav,
        nav_direction: nav&.right,
-       tick_count: tick_count,
        x_range: (player.x..player.x + tile_width),
        y_range: (player.y - tile_height..player.y)
   end
 
-  def go_south(nav:, tick_count:)
+  def go_south(args, nav:)
+    player.state.nav.down = args.tick_count
     go nav: nav,
        nav_direction: nav&.down,
-       tick_count: tick_count,
        x_range: ((player.x + 1) - tile_width..player.x),
        y_range: ((player.y) - tile_height..player.y)
   end
 
-  def go_west(nav:, tick_count:)
+  def go_west(args, nav:)
+    player.state.nav.left = args.tick_count
     go nav: nav,
        nav_direction: nav&.left,
-       tick_count: tick_count,
        x_range: (player.x - tile_width..player.x - 1),
        y_range: (player.y + 1..player.y + tile_height)
   end
@@ -66,24 +61,24 @@ class PlayerController
     player.fallen!
   end
 
-  def go(nav:, nav_direction:, tick_count:, x_range:, y_range:)
+  def go(nav:, nav_direction:, x_range:, y_range:)
     return unless nav_direction
-    return unless within_threshold?(nav_direction, tick_count: tick_count)
 
     clear_nav! nav
 
-    candidate = next_tile x_range: x_range, y_range: y_range
+    return unless within_threshold?(nav_direction)
 
+    candidate = next_tile x_range: x_range, y_range: y_range
     if candidate
       move! x: candidate.x, y: candidate.y
     else
-      move! x: x_range.to_a.max, y: y_range.to_a.max
+      move! x: x_range.to_a.sample, y: y_range.to_a.sample
       fall!
     end
   end
 
   def key_down(args)
-    @key_down ||= args.inputs.keyboard.key_down
+    args.inputs.keyboard.key_down
   end
 
   def move!(x:, y:)
@@ -105,11 +100,11 @@ class PlayerController
     @tile_height ||= world.tiles.first.h
   end
 
-  def within_threshold?(integer_or_array, tick_count:, threshold: 300.0)
-    integer_or_array = [integer_or_array] if integer_or_array.is_a?(Integer)
+  def within_threshold?(integer_or_array, threshold: 10)
+    array = [integer_or_array] if integer_or_array.is_a?(Integer)
 
-    return false if integer_or_array.nil? || integer_or_array.any?(&:nil?)
+    return false if array&.any?(&:nil?)
 
-    integer_or_array.all? { |n| (tick_count - n) < threshold.to_i }
+    array.all? { |n| ($args.tick_count - n) <= threshold }
   end
 end
--- app/tests.rb
+++ app/tests.rb
@@ -1,1 +1,2 @@
 require "app/models/world_test.rb"
+require "app/models/player_controller_test.rb"
