5e524f35854c850c27044fff8ac794f0201bd167
benjamin wil <hey@benjaminwil.info>
2023-04-02 14:48:55 -0700

    refactor controller test
    let's use `Level`s and cool test helper methods.

--- app/models/player_controller_test.rb
+++ app/models/player_controller_test.rb
@@ -1,51 +1,32 @@
-require "app/models/player_controller.rb"
-
-def player(x:, y:, w:, h:, state:)
-  @player ||= Player.new state: state, x: x, y: y, w: h, h: h
-end
-
-def controller(player:, world:)
-  @controller ||= PlayerController.new player: player, world: world
-end
-
-def world(args)
-  @world ||=
-    World
-      .new(screen_width: 6,
-           screen_height: 6,
-           rows: [1,2,3], tile_size: 2)
-      .tap { |world| world.set_background_colour!(args)
-                     world.render_tiles!(args) }
-end
-
-def render!(args, world:, player:)
-  world.tap { |w| w.set_background_colour! args
-                  w.render_tiles! args }
-  player.render! args
-end
+require "app/models/level.rb"
 
 def test_player_position args, assert
-  world_one = world args
-  player = player x: world_one.tiles.first.x,
-                  y: world_one.tiles.first.y,
-                  w: world_one.tile_size,
-                  h: world_one.tile_size,
-                  state: args.state.player_one
-
-  render! args, player: player, world: world_one
-  controller(player: player, world: world_one).tap { |c|
-    c.control! args
-  }
+  test_level =
+    Level.generate! args,
+                    world_params: {
+                      screen_width: 6,
+                      screen_height: 6,
+                      rows: [1, 2, 3],
+                      tile_size: 2
+                    }
+  player = test_level.player
 
   assert.equal! [player.x, player.y], [0, 0]
 
-  args.inputs.keyboard.key_down.up = true
-  5.times do
-    tick args
-    args.state.tick_count += 1
+  T.before_tick args do
+    test_level.execute!
+    args.inputs.keyboard.key_down.up = true
+  end
+
+  T.before_tick args, count: 5 do
+    test_level.execute!
     args.inputs.keyboard.key_down.up = false
   end
-  args.inputs.keyboard.key_up.up = true
+
+  T.before_tick args do
+    test_level.execute!
+    args.inputs.keyboard.key_up.up = true
+  end
 
   assert.equal! [player.x, player.y], [1, 2]
 end
--- app/tests.rb
+++ app/tests.rb
@@ -1,2 +1,16 @@
-require "app/models/world_test.rb"
+# Reusable test utility methods.
+#
+class T
+  class << self
+    def before_tick args, count: 1, &block
+      count.times do
+        yield
+        args.state.tick_count += 1
+        tick args
+      end
+    end
+  end
+end
+
 require "app/models/player_controller_test.rb"
+require "app/models/world_test.rb"
